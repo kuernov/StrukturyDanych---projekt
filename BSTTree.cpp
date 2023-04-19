cba611aeb66f451a1e9e93d46cc7a1ddb93366dc
Bartosz Małyjasiak <bartosz.malyjasiak@gmail.com>
2023-04-20 00:14:04 +0200

    added functions without implementation

--- BSTTree.cpp
+++ BSTTree.cpp
@@ -2,11 +2,42 @@
 
 using namespace std;
 
-class BSTTree {
-private:
-	struct Node {
-		int* right, left;
-		int value;
-	};
+struct Node {
+	int* right, left;
+	int value;
 };
 
+class BSTTree {
+private:
+	Node node;
+	Node* root;
+public:
+	BSTTree(int value);
+	void insert(int value);
+	void remove(int value);
+	Node search(int value);
+	bool isEmpty();
+
+};
+
+BSTTree::BSTTree(int value)
+{
+}
+
+void BSTTree::insert(int value)
+{
+}
+
+void BSTTree::remove(int value)
+{
+}
+
+Node BSTTree::search(int value)
+{
+	return Node();
+}
+
+bool BSTTree::isEmpty()
+{
+	return root == nullptr;
+}
